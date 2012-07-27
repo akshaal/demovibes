c64f9f44aedd1c5e8ab514b7786ba1501dad0a43
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2012-07-27 23:46:41 +0300

    XML: escape tag names. ../xml/songtypes/ added.
    Code beautification.

--- demovibes/webview/xml_urls.py
+++ demovibes/webview/xml_urls.py
@@ -1,7 +1,11 @@
+## Imports
+
 from django.conf.urls.defaults import *
 from demovibes.webview.xml_views import *
 from django.views.generic.list_detail import object_detail, object_list
 from tagging.models import Tag
+
+## Dicts
 
 song_dict = {
     'queryset': Song.objects.all(),
@@ -13,6 +17,12 @@ tag_dict = {
     'queryset': Tag.objects.all(),
     'mimetype': "application/xml",
     'template_name': 'webview/xml/tags.xml',
+}
+
+songtype_dict = {
+    'queryset': SongType.objects.all(),
+    'mimetype': "application/xml",
+    'template_name': 'webview/xml/songtypes.xml',
 }
 
 stream_dict = {
@@ -45,23 +55,32 @@ user_dict = {
     'template_name': 'webview/xml/user.xml',
 }
 
+## Methods
+
 def cached_object_list(*args, **kwargs):
     return object_list(*args, **kwargs)
 
 def cached_object_detail(*args, **kwargs):
     return object_detail(*args, **kwargs)
 
-urlpatterns = patterns('',
-    (r'^queue/$',                   'demovibes.webview.xml_views.queue'),
-    (r'^oneliner/$',                'demovibes.webview.xml_views.oneliner'),
-    (r'^online/$',                  'demovibes.webview.xml_views.online'),
-    (r'^song/(?P<songid>\d+)/$', SongInfo()),
-    (r'^compilation/(?P<object_id>\d+)/$', cached_object_detail , compilation_dict),
-    (r'^group/(?P<object_id>\d+)/$', cached_object_detail , group_dict),
-    (r'^artist/(?P<object_id>\d+)/$', cached_object_detail, artist_dict),
-    (r'^streams/$',                 cached_object_list, stream_dict),
-    (r'^tags/$',                 cached_object_list, tag_dict),
-    #(r'^artist/$',     'demovibes.webview.xml_views.artist'),
-    (r'^user/(?P<username>\w+)/$', UserInfo()),
-    (r'^user/(?P<username>\w+)/favorites/$', UserFavorites()),
+## URL patterns
+
+urlpatterns = patterns (
+    '',
+    (r'^queue/$',                                   'demovibes.webview.xml_views.queue'),
+    (r'^oneliner/$',                                'demovibes.webview.xml_views.oneliner'),
+    (r'^online/$',                                  'demovibes.webview.xml_views.online'),
+    (r'^song/(?P<songid>\d+)/$',                    SongInfo()),
+    (r'^compilation/(?P<object_id>\d+)/$',          cached_object_detail,   compilation_dict),
+    (r'^group/(?P<object_id>\d+)/$',                cached_object_detail,   group_dict),
+    (r'^artist/(?P<object_id>\d+)/$',               cached_object_detail,   artist_dict),
+    (r'^streams/$',                                 cached_object_list,     stream_dict),
+    (r'^tags/$',                                    cached_object_list,     tag_dict),
+    (r'^songtypes/$',                               cached_object_list,     songtype_dict),
+    #(r'^artist/$',                                 'demovibes.webview.xml_views.artist'),
+    (r'^user/(?P<username>\w+)/$',                  UserInfo()),
+    (r'^user/(?P<username>\w+)/favorites/$',        UserFavorites()),
 )
+
+## Local words for flyspell
+#  LocalWords:  xml queryset oneliner online username songtypes
